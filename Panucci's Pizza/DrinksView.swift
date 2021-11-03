e9aea5cd37618296498c7884dad8bd50ab54cb68
Michael Adams <me@michdavidadams.com>
2021-11-03 18:24:41 -0400

    Update DrinksView.swift

--- Panucci's Pizza/DrinksView.swift
+++ Panucci's Pizza/DrinksView.swift
@@ -9,46 +9,48 @@ import SwiftUI
 
 struct DrinksView: View {
     var body: some View {
-        LazyVStack {
-            ForEach(allDrinks) { drink in
-                HStack {
-                    Image(drink.image)
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .clipShape(Circle())
-                        .frame(width: 100, height: 100)
-                        .padding()
-                    
-                    VStack {
-                        Text(drink.name)
-                            .fontWeight(.bold)
-                            .lineLimit(2)
-                            .frame(width: 100.0)
-                            .dynamicTypeSize(.large)
-                            .multilineTextAlignment(.center)
+        ScrollView {
+            LazyVStack {
+                ForEach(allDrinks) { drink in
+                    HStack {
+                        Image(drink.image)
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .clipShape(Circle())
+                            .frame(width: 100, height: 100)
+                            .padding()
                         
-                        Text("$\(drink.price) per drink")
-                            .fontWeight(.semibold)
-                            .dynamicTypeSize(.medium)
+                        VStack {
+                            Text(drink.name)
+                                .fontWeight(.bold)
+                                .lineLimit(2)
+                                .frame(width: 100.0)
+                                .dynamicTypeSize(.large)
+                                .multilineTextAlignment(.center)
+                            
+                            Text("$\(drink.price) per drink")
+                                .fontWeight(.semibold)
+                                .dynamicTypeSize(.medium)
+                        }
+                        .padding(.horizontal)
+                        
+                        Text(drink.description)
+                            .fontWeight(.ultraLight)
+                            .dynamicTypeSize(.small)
+                            .multilineTextAlignment(.leading)
+                            .frame(width: 180.0)
+                        
+                        Button(action: {
+                            print("\(drink.name) added to order. ")
+                            updateDrinks(addedDrink: drink)
+                        }) {
+                            Text("Add to order")
+                        }
+                        .padding()
+                        
                     }
-                    .padding(.horizontal)
-                    
-                    Text(drink.description)
-                        .fontWeight(.ultraLight)
-                        .dynamicTypeSize(.small)
-                        .multilineTextAlignment(.leading)
-                        .frame(width: 180.0)
-                    
-                    Button(action: {
-                        print("\(drink.name) added to order. ")
-                        updateDrinks(addedDrink: drink)
-                    }) {
-                        Text("Add to order")
-                    }
-                    .padding()
-                    
+                    Divider()
                 }
-                Divider()
             }
         }
     }
